dc9b0368a27dd86963c38eed79b6e3ab8f674fdd
havana1712 <namnangno.des@gmail.com>
2023-03-21 06:27:54 +0000

    [LeetCode Sync] Runtime - 51 ms (6.30%), Memory - 43 MB (100.00%)

---
+++ my-folder/0387-first-unique-character-in-a-string/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        for (int i = 0;i<s.length();i++){
+            boolean unique=true;
+            for (int j=0;j<s.length();j++){
+                if (j==i){
+                    continue;
+                }
+                if (s.charAt(i)==s.charAt(j)){
+                    unique=false;
+                    break;
+                }
+            }
+            if (unique){
+               return i;
+            }
+        }
+        return -1;
+    }
+}
