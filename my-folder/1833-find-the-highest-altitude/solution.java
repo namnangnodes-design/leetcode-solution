2096431fa14e8c952f1069219e53de706d77875e
havana1712 <namnangno.des@gmail.com>
2023-09-27 13:38:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.5 MB (100.00%)

---
+++ my-folder/1833-find-the-highest-altitude/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max = 0;
+        int cur = 0;
+        for (int i = 0; i < gain.length; i++) {
+            cur += gain[i];
+            max = Math.max(cur, max);
+        }
+        return max;
+    }
+}
