bbe3088cbd166efd33c6c9f14da6da28cbec6a41
havana1712 <namnangno.des@gmail.com>
2024-05-04 16:15:28 +0000

    [LeetCode Sync] Runtime - 6 ms (5.72%), Memory - 10.1 MB (99.84%)

---
+++ my-folder/0046-permutations/solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> temp;
+    vector<bool> visited;
+    vector<vector<int>> result;
+    void backtrack(int n, vector<int>& nums)
+    {
+        if (n == nums.size())
+        {
+            result.push_back(temp);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (!visited.at(i))
+            {
+                visited.at(i) = true;
+                temp.push_back(nums.at(i));
+                backtrack(n + 1, nums);
+                temp.pop_back();
+                visited.at(i) = false;
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            visited.push_back(false);
+        }
+        backtrack(0, nums);
+        return result;
+    }
+};
