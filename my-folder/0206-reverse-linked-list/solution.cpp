2b0b9b66fa57651115514895ffea817ef633bc10
havana1712 <namnangno.des@gmail.com>
2024-08-04 04:13:11 +0000

    [LeetCode Sync] Runtime - 8 ms (0.70%), Memory - 13.2 MB (93.47%)

---
+++ my-folder/0206-reverse-linked-list/solution.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+ListNode* reverseList(ListNode* head) {
+     if (head == nullptr || head->next == nullptr)
+     {
+         return head;
+     }
+     ListNode* cur = head;
+     stack<ListNode*> nodeStack;
+     while (cur->next != nullptr)
+     {
+         nodeStack.push(cur);
+         cur = cur->next;
+     }
+     nodeStack.push(cur);
+     cur = nodeStack.top();
+     head = cur;
+     nodeStack.pop();
+     while (!nodeStack.empty())
+     {
+        cur->next = nodeStack.top();
+        nodeStack.pop();
+        cur = cur->next;
+     }
+     cur->next = nullptr;
+     return head;
+ }
+};
