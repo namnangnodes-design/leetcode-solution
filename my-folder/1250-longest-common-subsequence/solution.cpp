2dd7a6e2ab4a38ad948f837904f1df11426d1eb4
havana1712 <namnangno.des@gmail.com>
2025-02-09 15:37:17 +0000

    [LeetCode Sync] Runtime - 27 ms (57.02%), Memory - 27.3 MB (70.07%)

---
+++ my-folder/1250-longest-common-subsequence/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.length(), n = text2.length();
+        
+        // Use vector for dynamic sizing
+        vector<vector<int>> DP(m + 1, vector<int>(n + 1, 0));
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    DP[i][j] = DP[i - 1][j - 1] + 1;
+                } else {
+                    DP[i][j] = max(DP[i - 1][j], DP[i][j - 1]);
+                }
+            }
+        }
+        return DP[m][n];
+    }
+};
+
