59095722896e4c800f6bdd97a636c11523dc63ca
havana1712 <namnangno.des@gmail.com>
2025-03-12 14:41:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.7 MB (14.64%)

--- my-folder/1894-merge-strings-alternately/solution.cpp
+++ my-folder/1894-merge-strings-alternately/solution.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    std::string mergeAlternately(std::string word1, std::string word2) {
-        std::string ret = "";
-        int i = 0;
-        while (i < word1.length() && i < word2.length()) {
-            ret.push_back(word1.at(i));
-            ret.push_back(word2.at(i));
-            i++;
+    string mergeAlternately(string word1, string word2) {
+        int s1 = word1.size();
+        int s2 = word2.size();
+
+        string s;
+        s.reserve(s1 + s2);  // Preallocate memory to avoid reallocation overhead
+
+        int i = 0, j = 0;
+        while (i < s1 || j < s2) {
+            if (i < s1) s.push_back(word1[i++]);  // Use push_back instead of +=
+            if (j < s2) s.push_back(word2[j++]);  
         }
-        while (i < word1.length()) {
-            ret.push_back(word1[i]);
-            i++;
-        }
-        while (i < word2.length()) {
-            ret.push_back(word2[i]);
-            i++;
-        }
-        return ret;
+        return s;
     }
 };
+
