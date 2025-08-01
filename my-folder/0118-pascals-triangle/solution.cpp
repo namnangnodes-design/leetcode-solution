718cb389beb60c7857b711bc28684d0716639607
havana1712 <namnangno.des@gmail.com>
2025-08-01 04:02:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10 MB (9.41%)

--- my-folder/0118-pascals-triangle/solution.cpp
+++ my-folder/0118-pascals-triangle/solution.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
+    vector<vector<int>> pascalTriangle = { {1
+        }, {1, 1} };
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> result;
-        for (int i = 0; i < numRows; i++)
+        if (pascalTriangle.size() >= numRows)
         {
-            if (i == 0)
+            for (int i = 0; i < numRows; i++)
             {
-                result.push_back({ 1 });
+                result.push_back(pascalTriangle[i]);
             }
-            else if (i == 1)
-            {
-                result.push_back({ 1, 1 });
-            }
-            else
-            {
-                vector<int> temp;
-                temp.push_back(1);
-                for (int j = 1; j < result[i - 1].size(); j++)
-                {
-                    temp.push_back(result[i - 1][j - 1] + result[i - 1][j]);
-                }
-                temp.push_back(1);
-                result.push_back(temp);
-            }
+            return result;
         }
-        return result;
+        for (int i = pascalTriangle.size() - 1; i < numRows - 1; i++)
+        {
+            vector<int> row = { 1 };
+            for (int j = 1; j < pascalTriangle[i].size(); j++)
+            {
+                row.push_back(pascalTriangle[i][j] + pascalTriangle[i][j - 1]);
+            }
+            row.push_back(1);
+            pascalTriangle.push_back(row);
+        }
+        return pascalTriangle;
     }
 };
