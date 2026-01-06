313d22f63b5c83e0db56376e3e7ca30b274bf7ba
havana1712 <namnangno.des@gmail.com>
2026-01-06 11:26:37 +0000

    [LeetCode Sync] Runtime - 4 ms (72.85%), Memory - 14.3 MB (95.55%)

--- my-folder/0188-best-time-to-buy-and-sell-stock-iv/solution.cpp
+++ my-folder/0188-best-time-to-buy-and-sell-stock-iv/solution.cpp
@@ -1,69 +1,21 @@
 class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
-        int start = 0;
-        int end = 0;
-        vector<pair<int, int>> trans;
-        for (int i = 1; i < prices.size(); i++)
+        vector<int> buy;
+        vector<int> sell;
+        for (int i = 0; i <= k; i++)
         {
-            if (prices[i] > prices[i - 1])
+            buy.push_back(INT_MIN + 1001);
+            sell.push_back(0);
+        }
+        for (int price: prices)
+        {
+            for (int i = 1; i <= k; i++)
             {
-                end = i;
-            }
-            else
-            {
-                if (end > start)
-                {
-                    pair<int, int> newTran = {start, end};
-                    trans.push_back(newTran);
-                }
-                start = i;
+                buy[i] = max(buy[i], sell[i - 1] - price);
+                sell[i] = max(sell[i], buy[i] + price); 
             }
         }
-        if (end > start)
-        {
-            pair<int, int> newTran = {start, end};
-            trans.push_back(newTran);
-        }
-        int numOfTrans = trans.size();
-        while (numOfTrans > k)
-        {
-            int minLossDelete = INT_MAX;
-            int deleteIdx = -1;
-            for (int i = 0; i < trans.size(); i++)
-            {
-                if (minLossDelete > prices[trans[i].second] - prices[trans[i].first])
-                {
-                    minLossDelete = prices[trans[i].second] - prices[trans[i].first];
-                    deleteIdx = i;
-                }
-            }
-            int minLossMerge = INT_MAX;
-            int mergeIdx = -1;
-            for (int i = 0; i < trans.size() - 1; i++)
-            {
-                if (minLossMerge > prices[trans[i].second] - prices[trans[i + 1].first])
-                {
-                    minLossMerge = prices[trans[i].second] - prices[trans[i + 1].first];
-                    mergeIdx = i;
-                }
-            }
-            if (minLossMerge < minLossDelete)
-            {
-                trans[mergeIdx].second = trans[mergeIdx + 1].second; 
-                trans.erase(trans.begin() + mergeIdx + 1);
-            }
-            else
-            {
-                trans.erase(trans.begin() + deleteIdx);
-            }
-            numOfTrans--;
-        }
-        int profit = 0;
-        for (int i = 0; i < trans.size(); i++)
-        {
-            profit += prices[trans[i].second] - prices[trans[i].first];
-        }
-        return profit;
+        return sell[k];
     }
 };
