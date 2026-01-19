4dfaf71c4598bd57238d3be7c4b941e2fb2ed2fa
havana1712 <namnangno.des@gmail.com>
2026-01-19 14:25:22 +0000

    [LeetCode Sync] Runtime - 19 ms (0.00%), Memory - 15.3 MB (100.00%)

--- my-folder/1896-maximum-score-from-performing-multiplication-operations/solution.c
+++ my-folder/1896-maximum-score-from-performing-multiplication-operations/solution.c
@@ -1,44 +1,26 @@
-int memo[301][100001];
-int ns[100001];
-int ms[301];
-int n;
-int m;
+int memo[302][302];
 
 int max(int a, int b)
 {
     return a > b ? a : b;
 }
 
-int dp(int action, int left)
-{
-    if (action == m)
-    {
-        return 0;
-    }
-    if (memo[action][left] == 0)
-    {
-        memo[action][left] = max(dp(action + 1, left + 1) + ms[action] * ns[left], dp(action + 1, left) + ms[action] * ns[n - 1 - action + left]);
-    }
-    return memo[action][left];
-}
-
 int maximumScore(int* nums, int numsSize, int* multipliers, int multipliersSize) {
-    for (int i = 0; i < numsSize; i++)
+    for (int i = 0; i <= multipliersSize; i++)
     {
-        ns[i] = nums[i];
-    }
-    for (int i = 0; i < multipliersSize; i++)
-    {
-        ms[i] = multipliers[i];
-    }
-    for (int i = 0; i < multipliersSize; i++)
-    {
-        for (int j = 0; j < multipliersSize; j++)
+        for (int j = 0; j <= multipliersSize; j++)
         {
             memo[i][j] = 0;
         }
     }
-    m = multipliersSize;
-    n = numsSize;
-    return dp(0, 0);
+    for (int i = multipliersSize - 1; i >= 0; i--)
+    {
+        for (int left = i; left >= 0; left--)
+        {
+            int right = numsSize - 1 + left - i;
+            memo[i][left] = max(memo[i + 1][left + 1] + multipliers[i] * nums[left], memo[i + 1][left] + multipliers[i] * nums[right]);
+        }
+    }
+        
+    return memo[0][0];
 }
