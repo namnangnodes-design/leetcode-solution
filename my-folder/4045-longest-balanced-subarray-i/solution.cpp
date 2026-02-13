276947020343c8033902bd7099b2bed4a93ddcb8
havana1712 <namnangno.des@gmail.com>
2026-02-13 16:48:09 +0000

    [LeetCode Sync] Runtime - 1908 ms (55.60%), Memory - 530.2 MB (45.73%)

--- my-folder/4045-longest-balanced-subarray-i/solution.cpp
+++ my-folder/4045-longest-balanced-subarray-i/solution.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int longestBalanced(vector<int>& nums) {
-        size_t len = 0;
-
-        for (size_t i = 0; i < nums.size(); i++) {
-            auto odd = unordered_map<int, int>();
-            auto even = unordered_map<int, int>();
-
-            for (size_t j = i; j < nums.size(); j++) {
-                auto& c = (nums[j] & 1) ? odd : even;
-                c[nums[j]]++;
-
-                if (odd.size() == even.size()) {
-                    len = std::max(len, j - i + 1);
+        int res = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            unordered_set<int> odd;
+            unordered_set<int> even;
+            for (int j = i ; j < nums.size(); j++)
+            {
+                if (nums[j] % 2 == 0)
+                {
+                    odd.insert(nums[j]);
+                }
+                else
+                {
+                    even.insert(nums[j]);
+                }
+                if (odd.size() == even.size())
+                {
+                    res = max(res, j - i + 1);
                 }
             }
         }
-
-        return int(len);
+        return res;
     }
 };
