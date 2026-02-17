5eb23c862de430010182da85509bc32ff78cc2a6
havana1712 <namnangno.des@gmail.com>
2026-02-17 12:24:19 +0000

    [LeetCode Sync] Runtime - 31 ms (6.12%), Memory - 11.7 MB (91.22%)

--- my-folder/1250-longest-common-subsequence/solution.c
+++ my-folder/1250-longest-common-subsequence/solution.c
@@ -1,32 +1,33 @@
-int dp[1002][1002];
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
 
 int longestCommonSubsequence(char* text1, char* text2) {
-    for (int i = 0; text1[i] != 0; i++)
+    int DP[1002][1002];
+    int m = strlen(text1);
+    int n = strlen(text2);
+
+    for (int i = 1; i <= m; i++)
     {
-        dp[i][0] = 0; 
+        for (int j = 1; j <= n; j++)
+        {
+            DP[i][j] = 0;
+        }
     }
-    for (int i = 0; text2[i] != 0; i++)
+    for (int i = 1; i <= m; i++)
     {
-        dp[0][i] = 0; 
-    }
-    int i = 1;
-    int j = 1;
-    while (text1[i - 1] != 0)
-    {
-        j = 1;
-        while (text2[j - 1] != 0)
+        for (int j = 1; j <= n; j++)
         {
             if (text1[i - 1] == text2[j - 1])
             {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
+                DP[i][j] = max(DP[i - 1][j - 1] + 1, max(DP[i - 1][j], DP[i][j - 1]));
             }
             else
             {
-                dp[i][j] = dp[i - 1][j] > dp[i][j - 1] ? dp[i - 1][j] : dp[i][j - 1];
+                DP[i][j] = max(DP[i - 1][j], DP[i][j - 1]);
             }
-            j++;
         }
-        i++;
     }
-    return dp[i - 1][j - 1];
+    return DP[m][n];
 }
