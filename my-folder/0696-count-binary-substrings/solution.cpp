7eaa26841f2e8913befed65c0cdf62b78c349baa
havana1712 <namnangno.des@gmail.com>
2026-02-20 16:11:31 +0000

    [LeetCode Sync] Runtime - 5 ms (35.69%), Memory - 13.1 MB (25.87%)

---
+++ my-folder/0696-count-binary-substrings/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int count = 0;
+        int prev = 0;
+        int cur = 1;
+        for (int i = 1; i < s.size(); i++)
+        {
+            if (s[i] == s[i - 1])
+            {
+                cur++;
+            }
+            else
+            {
+                count += min(cur, prev);
+                prev = cur;
+                cur = 1;
+            }
+        }
+        count += min(cur, prev);
+        return count;
+    }
+};
