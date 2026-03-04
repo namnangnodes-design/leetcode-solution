767e678130b8f8334daefef6771ba6358000d301
havana1712 <namnangno.des@gmail.com>
2026-03-04 03:33:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.8 MB (24.12%)

--- my-folder/0198-house-robber/solution.cpp
+++ my-folder/0198-house-robber/solution.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int houseCount = nums.size();
-        if (houseCount == 1)
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        if (n == 2) return max(nums[0], nums[1]); 
+        vector<int> rob(n, 0);
+        rob[0] = nums[0];
+        rob[1] = nums[1];
+        rob[2] = max(nums[0] + nums[2], nums[1]);
+        for (int i = 3; i < n; i++)
         {
-            return nums[0];
+            rob[i] = max(max(rob[i - 2] + nums[i], rob[i - 1]), rob[i - 3] + nums[i]);
         }
-        if (houseCount < 3)
-        {
-            return max(nums[0], nums[1]);
-        }
-        int* dp = new int[houseCount];
-        dp[0] = nums[0];
-        dp[1] = nums[1];
-        dp[2] = dp[0] + nums[2];
-        for (int i = 3; i < houseCount; i++)
-        {
-            dp[i] = max( dp[i - 2] + nums[i], dp[i - 3] + nums[i]);
-        }
-        
-        return max(dp[houseCount - 1], dp[houseCount - 2]);
+        return max(rob[n - 1], rob[n - 2]);
     }
 };
