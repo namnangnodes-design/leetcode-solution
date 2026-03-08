2299570aa9dc7762d3871eee185c249df0fd95c9
havana1712 <namnangno.des@gmail.com>
2026-03-08 12:33:06 +0000

    [LeetCode Sync] Runtime - 2194 ms (5.02%), Memory - 506.2 MB (5.31%)

--- my-folder/0015-3sum/solution.cpp
+++ my-folder/0015-3sum/solution.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int target = 0;
         sort(nums.begin(), nums.end());
-        set<vector<int>> s;
-        vector<vector<int>> output;
-        for (int i = 1; i < nums.size() - 1; i++){
-            int j = i - 1;
-            int k = i + 1;
-            while (j >= 0 && k <= nums.size() - 1) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == target) {
-                    s.insert({nums[j], nums[i], nums[k]});
-                    k++;
-                    j--;
-                } else if (sum < target) {
-                    k++;
-                } else {
-                    j--;
+        vector<vector<int>> res;
+        set<vector<int>> setJ;
+        for (int i = 0; i < nums.size() - 2; i++)
+        {
+            int j = i + 1;
+            int k = nums.size() - 1;
+            //printf("nums[i] %d nums[j] %d nums[k] %d \n", nums[i], nums[j], nums[k]);
+            while (j < k)
+            {
+                int sum = nums[j] + nums[k];
+                if (sum < -nums[i])
+                {
+                    j++;
+                }
+                else if (sum > -nums[i])
+                {
+                    k--;
+                }
+                else 
+                {
+                    setJ.insert({nums[i], nums[j], nums[k]});
+                    j++;
                 }
             }
         }
-        for(auto triplets : s)
-            output.push_back(triplets);
-        return output;
+        for (auto x: setJ)
+        {
+            res.push_back(x);
+        }
+        return res;
     }
 };
