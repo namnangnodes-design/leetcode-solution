95083334c8ba98f6477abd24445f5316621b7752
havana1712 <namnangno.des@gmail.com>
2026-03-10 03:33:31 +0000

    [LeetCode Sync] Runtime - 2393 ms (0.00%), Memory - 168.3 MB (50.00%)

---
+++ my-folder/3407-find-all-possible-stable-binary-arrays-ii/solution.ts
@@ -0,0 +1,47 @@
+const MOD = 1000000007;
+
+function numberOfStableArrays(
+    zero: number,
+    one: number,
+    limit: number,
+): number {
+    let dp: number[][][] = Array.from({ length: zero + 1 }, () =>
+        Array.from({ length: one + 1 }, () => [0, 0]),
+    );
+
+    for (let i = 0; i <= zero; i++) {
+        for (let j = 0; j <= one; j++) {
+            for (let lastBit = 0; lastBit <= 1; lastBit++) {
+                if (i === 0) {
+                    if (lastBit === 0 || j > limit) {
+                        dp[i][j][lastBit] = 0;
+                    } else {
+                        dp[i][j][lastBit] = 1;
+                    }
+                } else if (j === 0) {
+                    if (lastBit === 1 || i > limit) {
+                        dp[i][j][lastBit] = 0;
+                    } else {
+                        dp[i][j][lastBit] = 1;
+                    }
+                } else if (lastBit === 0) {
+                    dp[i][j][lastBit] = dp[i - 1][j][0] + dp[i - 1][j][1];
+                    if (i > limit) {
+                        dp[i][j][lastBit] -= dp[i - limit - 1][j][1];
+                    }
+                } else {
+                    dp[i][j][lastBit] = dp[i][j - 1][0] + dp[i][j - 1][1];
+                    if (j > limit) {
+                        dp[i][j][lastBit] -= dp[i][j - limit - 1][0];
+                    }
+                }
+                dp[i][j][lastBit] %= MOD;
+                if (dp[i][j][lastBit] < 0) {
+                    dp[i][j][lastBit] += MOD;
+                }
+            }
+        }
+    }
+
+    return (dp[zero][one][0] + dp[zero][one][1]) % MOD;
+}
