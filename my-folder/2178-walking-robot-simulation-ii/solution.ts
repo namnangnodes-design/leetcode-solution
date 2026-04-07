2ec59aa109492a640d19f0e6867cc9d9a0e936e2
havana1712 <namnangno.des@gmail.com>
2026-04-07 10:50:14 +0000

    [LeetCode Sync] Runtime - 55 ms (40.00%), Memory - 70.4 MB (80.00%)

---
+++ my-folder/2178-walking-robot-simulation-ii/solution.ts
@@ -0,0 +1,48 @@
+class Robot {
+    private moved: boolean = false;
+    private idx: number = 0;
+    private pos: number[][] = [];
+    private dir: number[] = [];
+    private toDir: Map<number, string> = new Map();
+
+    constructor(width: number, height: number) {
+        this.toDir.set(0, "East");
+        this.toDir.set(1, "North");
+        this.toDir.set(2, "West");
+        this.toDir.set(3, "South");
+
+        for (let i = 0; i < width; ++i) {
+            this.pos.push([i, 0]);
+            this.dir.push(0);
+        }
+        for (let i = 1; i < height; ++i) {
+            this.pos.push([width - 1, i]);
+            this.dir.push(1);
+        }
+        for (let i = width - 2; i >= 0; --i) {
+            this.pos.push([i, height - 1]);
+            this.dir.push(2);
+        }
+        for (let i = height - 2; i > 0; --i) {
+            this.pos.push([0, i]);
+            this.dir.push(3);
+        }
+        this.dir[0] = 3;
+    }
+
+    step(num: number): void {
+        this.moved = true;
+        this.idx = (this.idx + num) % this.pos.length;
+    }
+
+    getPos(): number[] {
+        return this.pos[this.idx];
+    }
+
+    getDir(): string {
+        if (!this.moved) {
+            return "East";
+        }
+        return this.toDir.get(this.dir[this.idx])!;
+    }
+}
