22f61d4f4377d7653658c3adb273c95040def8cf
havana1712 <namnangno.des@gmail.com>
2026-04-09 11:09:17 +0000

    [LeetCode Sync] Runtime - 4 ms (3.92%), Memory - 10.6 MB (10.43%)

---
+++ my-folder/0410-split-array-largest-sum/solution.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int splitArray(vector<int>& weights, int days) {
+        int n = weights.size();
+        int sumW = 0;
+        int maxW = 1;
+        for (int i = 0; i < n; i++)
+        {
+            maxW = max(weights[i], maxW);
+            sumW += weights[i];
+        }
+        int res = sumW;
+        int l = maxW;
+        int r = sumW;
+        while (l <= r)
+        {
+            int m = l + (r - l) / 2;
+            int cur = 0;
+            int day = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (day == days)
+                {
+                    break;
+                }
+                if (cur + weights[i] > m)
+                {
+                    day++;
+                    cur = weights[i];
+                }
+                else
+                {
+                    cur += weights[i];
+                }
+            }
+            if (day < days)
+            {
+                res = min(res, m);
+                r = m - 1;
+            }
+            else
+            {
+                l = m + 1;
+            }
+        }
+        return res;
+    }
+};
