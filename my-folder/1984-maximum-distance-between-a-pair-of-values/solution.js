53b352b651a240c95fcf2d01ef034b3bdf98592a
havana1712 <namnangno.des@gmail.com>
2026-04-19 14:41:08 +0000

    [LeetCode Sync] Runtime - 2 ms (64.29%), Memory - 68.4 MB (57.14%)

---
+++ my-folder/1984-maximum-distance-between-a-pair-of-values/solution.js
@@ -0,0 +1,22 @@
+var maxDistance = function(nums1, nums2) {
+
+    let distance = 0;
+    let i = 0, j = 0;
+
+    while(j < nums2.length && i < nums1.length){
+
+        if(nums1[i] <= nums2[j]){
+            distance = Math.max(distance, j - i);
+            j++;
+        }
+        else if(i < j){
+            i++;
+        }
+        else{
+            i++;
+            j++;
+        }
+    }
+
+    return distance;
+};
