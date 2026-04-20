b6f69544d0fa7fdf011d25577b6beb634c4b19d0
havana1712 <namnangno.des@gmail.com>
2026-04-20 03:19:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.1 MB (39.46%)

---
+++ my-folder/2199-two-furthest-houses-with-different-colors/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int n = colors.size();
+        int res = 1;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (colors[i] != colors[j])
+                {
+                    res = max(res, j - i);
+                }
+            }
+        }
+        return res;
+    }
+};
