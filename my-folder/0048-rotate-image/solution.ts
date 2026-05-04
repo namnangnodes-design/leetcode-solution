52c3e605ede2ad449820f3f18f01e0ba41855535
havana1712 <namnangno.des@gmail.com>
2026-05-04 16:48:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 56.4 MB (48.54%)

---
+++ my-folder/0048-rotate-image/solution.ts
@@ -0,0 +1,12 @@
+function rotate(matrix: number[][]): void {
+    let n = matrix.length;
+    for (let i = 0; i < Math.floor((n + 1) / 2); i++) {
+        for (let j = 0; j < Math.floor(n / 2); j++) {
+            let temp = matrix[n - 1 - j][i];
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+            matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
+            matrix[j][n - 1 - i] = matrix[i][j];
+            matrix[i][j] = temp;
+        }
+    }
+}
