b5df6767f39b4a296014a1382c86e6391ed0a69e
havana1712 <namnangno.des@gmail.com>
2026-05-16 14:27:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 56 MB (31.15%)

---
+++ my-folder/0154-find-minimum-in-rotated-sorted-array-ii/solution.ts
@@ -0,0 +1,12 @@
+function findMin(nums: number[]): number {
+    let low = 0,
+        high = nums.length - 1;
+
+    while (low < high) {
+        let pivot = low + Math.floor((high - low) / 2);
+        if (nums[pivot] < nums[high]) high = pivot;
+        else if (nums[pivot] > nums[high]) low = pivot + 1;
+        else high -= 1;
+    }
+    return nums[low];
+}
