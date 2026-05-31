d77961e22d7b95d06b4662559c8bd5877c6d37f3
havana1712 <namnangno.des@gmail.com>
2026-05-31 11:19:48 +0000

    [LeetCode Sync] Runtime - 67 ms (58.06%), Memory - 14.5 MB (48.82%)

---
+++ my-folder/0300-longest-increasing-subsequence/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        int res = 1;
+        for (int i = 0; i < n; i++)
+        {
+            int temp = 1;
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[j] < nums[i]) temp = max(temp, dp[j] + 1);
+            }
+            dp[i] = temp;
+            res = max(res, dp[i]);
+        }
+        return res;
+    }
+};
