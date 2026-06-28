c1268df5d1f0883aa65d1a499f9afdf319507d3e
havana1712 <namnangno.des@gmail.com>
2026-06-28 16:15:28 +0000

    [LeetCode Sync] Runtime - 9 ms (43.61%), Memory - 55.3 MB (47.73%)

---
+++ my-folder/1956-maximum-element-after-decreasing-and-rearranging/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        arr[0] = 1;
+        for (int i = 1; i < n; i++)
+        {
+            arr[i] = min(arr[i - 1] + 1, arr[i]);
+        }  
+        return arr[n - 1];
+
+    }
+};
