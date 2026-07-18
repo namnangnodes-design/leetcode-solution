db5167130a4bb59abf49b27e81289473bc566870
havana1712 <namnangno.des@gmail.com>
2026-07-18 15:11:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.4 MB (34.84%)

--- my-folder/2106-find-greatest-common-divisor-of-array/solution.cpp
+++ my-folder/2106-find-greatest-common-divisor-of-array/solution.cpp
@@ -1,29 +1,12 @@
 class Solution {
 public:
     int findGCD(vector<int>& nums) {
-        int mMin = 1001, mMax = 0;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (nums[i] < mMin)
-            {
-                mMin = nums[i];
-            }
-            if (nums[i] > mMax)
-            {
-                mMax = nums[i];
-            }
+        int minV = nums[0];
+        int maxV = nums[0];
+        for (int num: nums)
+        {minV = min(minV, num);
+        maxV = max(maxV, num);
         }
-        int mMod = 1000;
-        while (mMin != 1)
-        {
-            mMod = mMax % mMin;
-            if (mMod == 0)
-            {
-                return mMin;
-            }
-            mMax = mMin;
-            mMin = mMod;
-        }
-        return mMin;
+        return gcd(minV, maxV);
     }
 };
